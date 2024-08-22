3001f71d145b2a155c99924db5bc697f8d66a923
Ryan <mathewsryan2@gmail.com>
2024-08-22 16:00:03 -0500

    added more questions

--- main.py
+++ main.py
@@ -10,15 +10,43 @@ class Window(QWidget):
 
     def ask_question(self):
         size, ok = QInputDialog.getInt(self, "Test", "How large would you like the password to be:")
+        upper, ok = QInputDialog.getInt(self, "Input Dialog", "How many upper case characters would you like:")
+        lower, ok = QInputDialog.getInt(self, "Input Dialog", "How many lower characters would you like:")
+        specialC, ok = QInputDialog.getInt(self, "Input Dialog", "How many special characters would you like:")
+        numC, ok = QInputDialog.getInt(self, "Input Dialog", "How many numbers would you like:")
+        
+
         if ok:
-            password = self.PasswordGenerator(size)
+            password = self.PasswordGenerator(size, upper, lower, specialC, numC)
             self.password_label.setText(f"Generated Password: {password}")
             
 
-    def PasswordGenerator(self, size):
+    def PasswordGenerator(self, size, upper, lower, specialC, numC):
         SpecialList = ["!", "@", "#", "$", "%", "^", "&", "*"]
-        password = ''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k=size))
-        return password
+        Lowercase_Letters = ''.join(random.choices(string.ascii_lowercase, k=lower))
+	#Generates an amount of uppercase characters
+        Uppercase_letters = ''.join(random.choices(string.ascii_uppercase, k=upper))
+	#Generates an amount of Numerical characters
+        Numerical = ''.join(random.choices(string.digits, k=numC))
+	#Generates an amount of Special characters			
+        Special_char = ''.join(random.choices(SpecialList, k=specialC))
+
+    #Generates the remaining characters needed for length
+        Length_char = ''.join([random.choices(string.ascii_letters)
+									for char in range(size  - (lower + upper + numC + specialC) )])
+			
+	#Concatenating the strings together
+        Newpassword = Lowercase_Letters + Uppercase_letters + Numerical + str(Special_char) + Length_char
+
+	#Turning the new string into a list for a list function
+        Newpassword_List = list(Newpassword)
+
+	#The list function shuffle moves the strings to different postions 
+        random.shuffle(Newpassword_List)
+			
+	#Joining the list back to a string
+        Final_Password = ''.join(Newpassword_List)
+        return Final_Password 
 
     def initUI(self):
         self.setWindowTitle("Password Generator App")
